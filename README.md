54e5994d88c33bce71f1ae15ff269c40609c4a70
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-09-01 14:51:30 +0100

    completed hello-world
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ https://medium.com/@povilasve/go-advanced-tips-tricks-a872503ac859
 ## References
 
 - [Array Tricks](https://github.com/golang/go/wiki/SliceTricks)
+- [Go Formatting](https://pkg.go.dev/fmt#hdr-Printing)
--- go-with-tests/hello-world/hello.go
+++ go-with-tests/hello-world/hello.go
@@ -2,9 +2,33 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, World!"
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
 }
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world", ""))
 }
