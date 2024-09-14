68158a183f42ea622f4e88ac3e9ae124a9f92f34
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-09-14 12:44:15 +0100

    completed concurency
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- go-with-tests/REAMD.md
+++ go-with-tests/REAMD.md
@@ -1,3 +1,7 @@
 # Go With Tests
 
 [Tutorial](https://quii.gitbook.io/learn-go-with-tests)
+
+```sh
+go test -race
+```
---
+++ go-with-tests/concurency/concurency.go
@@ -0,0 +1,33 @@
+package concurrency
+
+type WebsiteChecker func(string) bool
+
+func CheckWebsitesSequence(checker WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	for _, url := range urls {
+		results[url] = checker(url)
+	}
+	return results
+}
+
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(checker WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, checker(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		// Receive expression
+		res := <-resultChannel
+		results[res.string] = res.bool
+	}
+	return results
+}
