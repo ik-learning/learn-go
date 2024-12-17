01ab1a9e23f47c0c22cdfab4403df6e3753384ae
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2024-12-17 11:14:08 +0000

    wip
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

---
+++ go-with-tests/generics/stack.go
@@ -0,0 +1,74 @@
+package generics
+
+type StackOfInts struct {
+	values []int
+}
+
+func (s *StackOfInts) Push(v int) {
+	s.values = append(s.values, v)
+}
+
+func (s *StackOfInts) IsEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *StackOfInts) Pop() (int, bool) {
+	if s.IsEmpty() {
+		return 0, false
+	}
+	index := len(s.values) - 1
+	el := s.values[index]
+	s.values = s.values[:index]
+	return el, true
+}
+
+type StackOfStrings struct {
+	values []string
+}
+
+func (s *StackOfStrings) Push(v string) {
+	s.values = append(s.values, v)
+}
+
+func (s *StackOfStrings) IsEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *StackOfStrings) Pop() (string, bool) {
+	if s.IsEmpty() {
+		return "", false
+	}
+
+	index := len(s.values) - 1
+	el := s.values[index]
+	s.values = s.values[:index]
+	return el, true
+}
+
+type Stack[T any] struct {
+	values []T
+}
+
+func (s *Stack[T]) Push(v T) {
+	s.values = append(s.values, v)
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	if s.IsEmpty() {
+		var zero T
+		return zero, false
+	}
+
+	index := len(s.values) - 1
+	el := s.values[index]
+	s.values = s.values[:index]
+	return el, true
+}
+
+func NewStack[T any]() *Stack[T] {
+	return new(Stack[T])
+}
